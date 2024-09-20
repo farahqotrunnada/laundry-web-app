fdb5302ec48c85fbd841aff96c1a04efe5349166
farahqotrunnada <64992777+farahqotrunnada@users.noreply.github.com>
2024-09-21 02:31:39 +0700

    fix: fix auth layout container size on smaller screen

--- apps/web/src/layouts/auth/auth.tsx
+++ apps/web/src/layouts/auth/auth.tsx
@@ -2,6 +2,7 @@ import AppIcon from '@/components/app-icon';
 import Background from '@/assets/background.jpg';
 import Image from 'next/image';
 import Link from 'next/link';
+import { PROJECT_NAME } from '@/lib/constant';
 import React from 'react';
 
 interface AuthProps extends React.PropsWithChildren {
@@ -10,17 +11,17 @@ interface AuthProps extends React.PropsWithChildren {
 
 const AuthLayout: React.FC<AuthProps> = ({ children }) => {
   return (
-    <div className='w-full lg:grid lg:grid-cols-2 h-screen relative'>
-      <Link href='/' className='absolute top-0 left-0 m-8 font-semibold gap-2 items-center flex'>
-        <AppIcon className='h-6 w-6' />
-        <span className=' whitespace-nowrap'>Laundry Express</span>
+    <div className='relative w-full h-screen lg:grid lg:grid-cols-2'>
+      <Link href='/' className='absolute top-0 left-0 flex items-center gap-2 font-semibold m-7'>
+        <AppIcon className='w-6 h-6' />
+        <span className=' whitespace-nowrap'>{PROJECT_NAME}</span>
       </Link>
 
-      <div className='flex items-center justify-center py-12 h-full lg:h-auto'>
+      <div className='container flex items-center justify-center h-full py-12 lg:h-auto'>
         <div className='mx-auto w-96'>{children}</div>
       </div>
 
-      <div className='hidden bg-muted lg:block overflow-hidden'>
+      <div className='hidden overflow-hidden bg-muted lg:block'>
         <Image
           src={Background}
           alt='Image'
