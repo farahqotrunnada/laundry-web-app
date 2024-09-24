dcd15b3ad4a4388cc619420de2e9c8ce397b0370
farahqotrunnada <farah.nada14@gmail.com>
2024-09-24 20:24:57 +0700

    fix: wording

--- apps/web/src/app/(employee)/dashboard/(outletadmin)/reports/page.tsx
+++ apps/web/src/app/(employee)/dashboard/(outletadmin)/reports/page.tsx
@@ -12,7 +12,7 @@ export default async function Page({}: PageProps): Promise<React.JSX.Element> {
       <div className='flex flex-col items-start space-y-2'>
         <h2 className='text-4xl font-bold'>Report Page</h2>
         <p className='leading-relaxed tracking-tight text-left text-muted-foreground'>
-          Managing a small business today is already tough. Avoid further complications by ditching outdated.
+          Get clear insights into your business performance. Track orders, employee distribution, and revenue trends with up-to-date, visualized reports.
         </p>
       </div>
 
